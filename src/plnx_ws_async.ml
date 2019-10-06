44bdd1bd2c00a8748143ba272ba610753c549bf5
Vincent Bernardoff <vb@luminar.eu.org>
2019-10-06 17:47:30 +0800

    fix compilation (fastws)

--- src/plnx_ws_async.ml
+++ src/plnx_ws_async.ml
@@ -7,29 +7,28 @@ open Plnx_ws
 let url = Uri.make ~scheme:"https" ~host:"api2.poloniex.com" ()
 
 let connect ?(buf=Bi_outbuf.create 4096) () =
-  Fastws_async.connect_ez url >>|
-  Result.map ~f:begin fun (r, w, cleaned_up) ->
-    let ws_read, client_write = Pipe.create () in
-    let client_read = Pipe.map r ~f:begin fun msg ->
-        (Yojson_encoding.destruct_safe encoding
-           (Yojson.Safe.from_string ~buf msg))
-      end in
-    don't_wait_for @@
-    Pipe.transfer ws_read w ~f:begin fun cmd ->
-      Yojson.Safe.to_string ~buf
-        (Yojson_encoding.construct command_encoding cmd)
-    end ;
-    client_read, client_write, cleaned_up
-  end
+  Deferred.Or_error.map (Fastws_async.EZ.connect url)
+    ~f:begin fun { r; w; cleaned_up } ->
+      let ws_read, client_write = Pipe.create () in
+      let client_read = Pipe.map r ~f:begin fun msg ->
+          (Yojson_encoding.destruct_safe encoding
+             (Yojson.Safe.from_string ~buf msg))
+        end in
+      don't_wait_for @@
+      Pipe.transfer ws_read w ~f:begin fun cmd ->
+        Yojson.Safe.to_string ~buf
+          (Yojson_encoding.construct command_encoding cmd)
+      end ;
+      client_read, client_write, cleaned_up
+    end
 
 let connect_exn ?buf () =
   connect ?buf () >>= function
-  | Error `Internal exn -> raise exn
-  | Error `WS e -> Fastws_async.raise_error e
+  | Error e -> Error.raise e
   | Ok a -> return a
 
 let with_connection ?(buf=Bi_outbuf.create 4096) f =
-  Fastws_async.with_connection_ez url ~f:begin fun r w ->
+  Fastws_async.EZ.with_connection url ~f:begin fun r w ->
     let client_read = Pipe.map r ~f:begin fun msg ->
         Yojson_encoding.destruct_safe encoding (Yojson.Safe.from_string ~buf msg)
       end in
@@ -43,7 +42,5 @@ let with_connection ?(buf=Bi_outbuf.create 4096) f =
 
 let with_connection_exn ?buf f =
   with_connection ?buf f >>= function
-  | Error `Internal exn
-  | Error `User_callback exn -> raise exn
-  | Error `WS e -> Fastws_async.raise_error e
+  | Error e -> Error.raise e
   | Ok a -> return a
--- src/plnx_ws_async.mli
+++ src/plnx_ws_async.mli
@@ -3,8 +3,7 @@ open Plnx_ws
 
 val connect :
   ?buf:Bi_outbuf.t -> unit ->
-  (t Pipe.Reader.t * command Pipe.Writer.t * unit Deferred.t,
-  [ `Internal of exn | `WS of Fastws_async.error ]) result Deferred.t
+  (t Pipe.Reader.t * command Pipe.Writer.t * unit Deferred.t) Deferred.Or_error.t
 
 val connect_exn :
   ?buf:Bi_outbuf.t -> unit ->
@@ -13,9 +12,7 @@ val connect_exn :
 val with_connection :
   ?buf:Bi_outbuf.t ->
   (t Pipe.Reader.t -> command Pipe.Writer.t -> 'a Deferred.t) ->
-  ('a, [ `Internal of exn
-       | `User_callback of exn
-       | `WS of Fastws_async.error ]) result Deferred.t
+  'a Deferred.Or_error.t
 
 val with_connection_exn :
   ?buf:Bi_outbuf.t ->
